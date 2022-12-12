101e53a339e9ad3a199f8b6ae4b24f859753a7a3
Aurélien <aurelien@rastel.fr>
2022-12-12 11:03:28 +0000

    day2: part2: apply same simple match

--- src/day2/main.rs
+++ src/day2/main.rs
@@ -22,12 +22,12 @@ use std::fs::read_to_string;
 ///
 fn main() -> std::io::Result<()> {
     let data_string = read_to_string("./data/day2.dat")?;
-    let result = rock_paper_scissors_sum(data_string);
+    let result = sum_games(data_string, calc_score_2);
     println!("{}", result);
     Ok(())
 }
 
-fn calc_points(s: &str) -> u32 {
+fn calc_score_1(s: &str) -> u32 {
     match s {
         // A is rock
         "A X" => 4, // 1 (rock)     + 3 (draw)
@@ -45,18 +45,46 @@ fn calc_points(s: &str) -> u32 {
     }
 }
 
-fn rock_paper_scissors_sum(string: String) -> u32 {
-    string.trim().lines().map(calc_points).sum()
+fn calc_score_2(s: &str) -> u32 {
+    match s {
+        // A is rock
+        "A X" => 3, // 1 (lose) -> Scissors -> 3 + 0
+        "A Y" => 4, // 2 (draw) -> Rock -> 1 + 3
+        "A Z" => 8, // 3 (win)  -> Paper -> 2 + 6
+        // B is paper
+        "B X" => 1, // 1 (lose) -> Rock -> 1 + 0
+        "B Y" => 5, // 2 (draw) -> Paper -> 2 + 3
+        "B Z" => 9, // 3 (win)  -> Scissors -> 3 + 6
+        // C is scissors
+        "C X" => 2, // 1 (lose) -> paper -> 2 + 0
+        "C Y" => 6, // 2 (draw) -> scissors -> 3 + 3
+        "C Z" => 7, // 3 (win)  -> Rock -> 1 + 6
+        _ => panic!(),
+    }
+}
+
+fn sum_games(string: String, method: fn(&str) -> u32) -> u32 {
+    string.trim().lines().map(method).sum()
 }
 
 #[test]
-fn test_example() {
+fn test_example_part1() {
     const INPUT: &str = "
 A Y
 B X
 C Z
 ";
-    let sum = rock_paper_scissors_sum(INPUT.to_string());
+    let sum = sum_games(INPUT.to_string(), calc_score_1);
     assert_eq!(sum, 15);
 }
 
+#[test]
+fn test_example_part2() {
+    const INPUT: &str = "
+A Y
+B X
+C Z
+";
+    let sum = sum_games(INPUT.to_string(), calc_score_2);
+    assert_eq!(sum, 12);
+}
