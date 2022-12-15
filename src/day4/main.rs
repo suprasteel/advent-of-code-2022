71d3d38c14fa56fb59bf7ada48edd4d2daaae243
Aurélien <aurelien@rastel.fr>
2022-12-15 20:19:08 +0000

    day4: fix overlaps boolean calculation (not gives the corrct result)

--- src/day4/main.rs
+++ src/day4/main.rs
@@ -1,6 +1,7 @@
 use std::{fs::read_to_string, num::ParseIntError};
 
 const VARIANT: u8 = 2;
+const LOG_LVL: log::Level = log::Level::Trace;
 
 const fn get_code_variant() -> fn(&str) -> usize {
     match VARIANT {
@@ -10,39 +11,38 @@ const fn get_code_variant() -> fn(&str) -> usize {
     }
 }
 
-fn debug_mapped_items<I: std::fmt::Debug>(item: I) -> I {
-    log::debug!("{:?}", item);
+fn trace_items<I: std::fmt::Debug>(item: I) -> I {
+    log::trace!("{:?}: {:?}", std::any::type_name::<I>(), item);
     item
 }
 
 fn main() -> std::io::Result<()> {
-    simple_logger::init_with_level(log::Level::Debug).unwrap();
+    simple_logger::init_with_level(LOG_LVL).unwrap();
+    log::info!("Running variant {} with log level {:?}", VARIANT, LOG_LVL);
     let string = read_to_string("./data/day4.dat")?;
     let c = string.lines().count();
-    let sum = get_code_variant()(string.as_str());
+    let sum = get_code_variant()(string.trim());
     log::info!("Result:  {} ({} lines)", sum, c);
-
-
     Ok(())
 }
 
 fn count_teams_having_complete_taks_inclusion(s: &str) -> usize {
     s.lines()
-        .map(debug_mapped_items)
+        .map(trace_items)
         .map(|line| ElvesTeam::parse(line).expect("failed to parse line"))
-        .map(debug_mapped_items)
+        .map(trace_items)
         .map(|team| if team.has_assign_inclusion() { 1 } else { 0 })
-        .map(debug_mapped_items)
+        .map(trace_items)
         .sum::<usize>()
 }
 
 fn count_teams_having_overlaping_tasks(s: &str) -> usize {
     s.lines()
-        .map(debug_mapped_items)
+        .map(trace_items)
         .map(|line| ElvesTeam::parse(line).expect("failed to parse line"))
-        .map(debug_mapped_items)
+        .map(trace_items)
         .map(|team| if team.has_assign_overlaps() { 1 } else { 0 })
-        .map(debug_mapped_items)
+        .map(trace_items)
         .sum::<usize>()
 }
 
@@ -66,7 +66,7 @@ impl Assignment {
     }
 
     fn overlaps(&self, other: &Assignment) -> bool {
-        self.0 <= other.0 && self.1 >= other.0 || self.1 >= other.0 && self.1 <= self.0
+        if other.0 > self.0 { other.0 <= self.1 } else { other.1 >= self.0 }
     }
 }
 
@@ -99,14 +99,14 @@ impl ElvesTeam {
                         if i == j {
                             acc
                         } else {
-                            let res = acc || left.overlaps(right);
+                            let overlaps = left.overlaps(right);
                             log::debug!(
                                 "Does {:?} overlaps {:?} ? The anwser is {}",
                                 left,
                                 right,
-                                left.overlaps(right)
+                                overlaps
                             );
-                            res
+                            acc || overlaps
                         }
                     })
             })
@@ -125,14 +125,14 @@ impl ElvesTeam {
                         if i == j {
                             acc
                         } else {
-                            let res = acc || left.includes(right);
+                            let includes = left.includes(right);
                             log::debug!(
                                 "Does {:?} includes {:?} ? The anwser is {}",
                                 left,
                                 right,
-                                left.includes(right)
+                                includes
                             );
-                            res
+                            acc || includes
                         }
                     })
             })
