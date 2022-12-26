935d59343ba887440984eeeefa2dba176ebe8ee9
Aurélien <aurelien@rastel.fr>
2022-12-26 16:25:02 +0000

    day7: part2: solved

--- src/day7/main.rs
+++ src/day7/main.rs
@@ -48,6 +48,9 @@ const FS_TREE_STR: &str = r#"- / (dir)
 
 fn main() -> std::io::Result<()> {
     let data = read_to_string("./data/day7.dat")?;
+
+    let disk_space = 70_000_000;
+    let need_space = 30_000_000;
     let (_, parsed_term) = terminal(data.as_str()).unwrap();
     let mut cmd_iterator = parsed_term.into_iter();
     let tree = build_tree(&mut cmd_iterator, vec![], None);
@@ -63,15 +66,26 @@ fn main() -> std::io::Result<()> {
     };
 
     let b = top.borrow();
-    let size = b.size();
+    let fs_size = b.size();
     let v = top.borrow().to_vec();
     let k100 = v.iter().fold(
         0,
         |acc, (n, s, d)| if *d && s < &100_000 { acc + s } else { acc },
     );
 
-    dbg!(size);
-    dbg!(k100);
+    let free_space = (disk_space - fs_size) as i64;
+    let missing_space = need_space - free_space;
+    let missing_space = (if missing_space < 0 { 0 } else { missing_space }) as u64;
+
+    let delete_folder = v.into_iter().filter(|elt| elt.1 > missing_space).fold(
+        ("".into(), disk_space, false),
+        |acc, elt| if elt.1 < acc.1 { elt } else { acc },
+    );
+
+    dbg!(fs_size);
+    dbg!(k100); // part1 rep
+    dbg!(missing_space);
+    dbg!(delete_folder); // part2 rep
 
     Ok(())
 }
